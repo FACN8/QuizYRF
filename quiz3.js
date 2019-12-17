ba44435442034ff72e02d751a1acc899a01fc877
Yousef <yousefrizik92@gmail.com>
2019-12-17 11:03:59 +0200

    adding files > relates #1

---
+++ quiz3.js
@@ -0,0 +1,24 @@
+function buildQuiz(){
+
+}
+
+function showResults(){
+
+}
+
+buildQuiz();
+
+
+
+submitButton.addEventListener('click', showResults);
+
+
+const myQuestions = [ {
+    question: "",
+    answers:{
+        a:"",
+        b:"",
+        c:""
+    },
+    correctAnswer: "c" 
+},
